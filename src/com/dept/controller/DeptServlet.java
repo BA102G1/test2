bc257eb1b9e016b8c3942c56dbbfeffbd83bb5a9
chris <chrisyen8341@gmail.com>
2017-08-24 05:03:46 +0800

    FK

--- src/com/dept/controller/DeptServlet.java
+++ src/com/dept/controller/DeptServlet.java
@@ -24,37 +24,7 @@ public class DeptServlet extends HttpServlet {
 		String action = req.getParameter("action");
 
 		    // 來自select_page.jsp的請求                                  // 來自 dept/listAllDept.jsp的請求
-		if ("listEmps_ByDeptno_A".equals(action) || "listEmps_ByDeptno_B".equals(action)) {
-
-			List<String> errorMsgs = new LinkedList<String>();
-			req.setAttribute("errorMsgs", errorMsgs);
-
-			try {
-				/*************************** 1.接收請求參數 ****************************************/
-				Integer deptno = new Integer(req.getParameter("deptno"));
-
-				/*************************** 2.開始查詢資料 ****************************************/
-				DeptService deptSvc = new DeptService();
-				Set<EmpVO> set = deptSvc.getEmpsByDeptno(deptno);
-
-				/*************************** 3.查詢完成,準備轉交(Send the Success view) ************/
-				req.setAttribute("listEmps_ByDeptno", set);    // 資料庫取出的set物件,存入request
-
-				String url = null;
-				if ("listEmps_ByDeptno_A".equals(action))
-					url = "/dept/listEmps_ByDeptno.jsp";        // 成功轉交 dept/listEmps_ByDeptno.jsp
-				else if ("listEmps_ByDeptno_B".equals(action))
-					url = "/dept/listAllDept.jsp";              // 成功轉交 dept/listAllDept.jsp
-
-				RequestDispatcher successView = req.getRequestDispatcher(url);
-				successView.forward(req, res);
-
-				/*************************** 其他可能的錯誤處理 ***********************************/
-			} catch (Exception e) {
-				throw new ServletException(e);
-			}
-		}
-		
+	
 		
 		if ("delete_Dept".equals(action)) { // 來自/dept/listAllDept.jsp的請求
 
